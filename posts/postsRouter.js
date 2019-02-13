781d14bd73649be0f34e8bc4f5e1d6f154baee57
joshua-disney <joshua.a.disney@gmail.com>
2019-02-13 01:37:02 -0700

    Set up server and router

---
+++ posts/postsRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const Posts = require('./postsModel.js');
+
+const router = express.Router();
+
+// router.use(express.json());
+
+// All my gets and puts and stuff goes here.
+// router.get('api/posts')  <--- example
+// All things that say '/api/posts' is now just '/'
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const postsRouter = require('./posts/postsRouter.js')
+
 const server = express();
 
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-  res.send(`
-  <h1>Code me, Disney</h1>
-  `);
+server.use('/api/posts', postsRouter)
+
+server.get('/', (req, res) => {
+  res.send('<h1>Code me, Disney</h1>');
 });
 
 module.exports = server;
